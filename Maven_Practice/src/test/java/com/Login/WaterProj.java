cee729de53327aa4c5e7a084bb81d8c84cb96250
Diya Bala <Diya Bala@DESKTOP-4G3ERNM.gateway.pace.com>
2017-12-05 13:48:31 -0600

    Testing

---
+++ Maven_Practice/src/test/java/com/Login/WaterProj.java
@@ -0,0 +1,14 @@
+package com.Login;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class WaterProj {
+	
+	public static void main(String[] args) {
+		
+		ChromeDriver driver=new ChromeDriver();
+		driver.get("http://www.ixl.com");
+		
+	}
+
+}
